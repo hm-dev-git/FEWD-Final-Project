3b70c46998a2043dae917e2c7577068ac20dedf8
hmd-git <hmd.node@gmail.com>
2014-06-12 17:31:44 -0400

    Season Switcher Works!!
    addoing in the positioning, and have to make it scroll down

--- Season Switcher/js/index.js
+++ Season Switcher/js/index.js
@@ -1,40 +1,18 @@
-// console.log(Date.today); 
-// interestingly, without the functionCall() it TELLS what it's doing, neat
+$("#spri").click(function() {
+	$("body").css({"background-image": "url(./images/spring.jpg)"})
+})
 
+$("#summ").click(function() {
+	$("body").css({"background-image": "url(./images/summer.jpg)"})
 
-// var month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+})
 
+$("#autm").click(function() {
+	$("body").css({"background-image": "url(./images/autumn.jpg)"})
 
-// console.log(Date.prototype.getMonth());
+})
 
-//src: http://jsfiddle.net/dstorey/Xgerq/
-var daToday = new Date.now(),
-    loc = "en-US";
-    
-function writeMonth(){
-	monthFull = daToday.toLocaleString(loc, { month: "long" });
-	console.log(monthFull); 
-	$("#title_month").html(monthFull);
-}
+$("#wint").click(function() {
+	$("body").css({"background-image": "url(./images/winter.jpg)"})
 
-
-
-function writeDaysOfWeek(){
-	// console.log(daToday.toLocaleString(loc, { weekday: "short"}));
-	for (var i = 0; i < 7; i++) {
-		var dateTrack = daToday;
-		var wkDay = daToday.toLocaleString(loc, { weekday: "short"});
-		$('#'+wkDay).html(dateTrack.getDate());
-
-		if (wkDay == "Sat") {
-			dateTrack.addDays(-6);
-		} else {
-			dateTrack.addDays(1);
-		}
-
-	}
-}
-
-
-writeMonth();
-writeDaysOfWeek();
+})
