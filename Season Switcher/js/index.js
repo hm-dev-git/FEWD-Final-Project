9de95d0adecb36bddb650eebff1d6add700e3678
hmd-git <hmd.node@gmail.com>
2014-06-12 18:07:41 -0400

    Final Commit for class
    simplified some JS so I don’t have so many repeating lines for getting
    rid of the h2 for the switcher, I need to get to class!!

--- Season Switcher/js/index.js
+++ Season Switcher/js/index.js
@@ -1,18 +1,19 @@
+$("li").click(function() {
+	$("h2").css("display","none");
+})
+
 $("#spri").click(function() {
-	$("body").css({"background-image": "url(./images/spring.jpg)"})
+	$("body").css({"background-image": "url(./images/spring.jpg)", "background-position": "0% 15%"});
 })
 
 $("#summ").click(function() {
-	$("body").css({"background-image": "url(./images/summer.jpg)"})
-
+	$("body").css({"background-image": "url(./images/summer.jpg)", "background-position": "0% 70%"});
 })
 
 $("#autm").click(function() {
-	$("body").css({"background-image": "url(./images/autumn.jpg)"})
-
+	$("body").css({"background-image": "url(./images/autumn.jpg)", "background-position": "0% 40%"});
 })
 
 $("#wint").click(function() {
-	$("body").css({"background-image": "url(./images/winter.jpg)"})
-
+	$("body").css({"background-image": "url(./images/winter.jpg)", "background-position": "0% 27%"});
 })
